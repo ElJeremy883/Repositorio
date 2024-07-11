a2a5c7a6fc8374deeb0a30c3acb17bed38db6992
kimpamela-1239506 <kimpameladiaz0613@gmail.com>
2024-07-11 12:16:25 -0600

    Inicio

---
+++ src/javaapplication54/JavaApplication54.java
@@ -0,0 +1,17 @@
+
+package javaapplication54;
+
+
+public class JavaApplication54 {
+
+
+    public static void main(String[] args) {
+    int valor = 10;
+    int total = 0;
+        for (int i = 0; i <= 10; i++) {
+        total = valor * i;
+            System.out.println(valor + "*" + i + "=" + total);
+        }
+    }
+    
+}
